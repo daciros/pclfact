68d946fe6e5ba64b0cd06ac78db0d87e90cd4364
daciros <daciros3284@gmail.com>
2024-09-11 18:56:51 -0500

    add mongo conecction

--- app.js
+++ app.js
@@ -1,42 +1,50 @@
 // app.js
-
-const express = require('express');
-const swaggerConfig = require('./config/swagger');
-const accountingRoutes = require('./routes/accountingRoutes');
-const clientRoutes = require('./routes/clientRoutes');
-const usersRoutes = require('./routes/usersRoutes');
-const inventoryRoutes = require('./routes/inventoryRoutes');
-const invoiceRoutes = require('./routes/invoiceRoutes');
-const orderRoutes = require('./routes/orderRoutes');
-const permissionRoutes = require('./routes/permissionRoutes');
-const productRoutes = require('./routes/productRoutes');
-const roleRoutes = require('./routes/roleRoutes');
-const stockRoutes = require('./routes/stockRoutes');
-const supplierRoutes = require('./routes/supplierRoutes');
-const supplyRoutes = require('./routes/supplyRoutes');
-
+const config = require("./config/config");
+const express = require("express");
+const swaggerConfig = require("./config/swagger");
+const accountingRoutes = require("./routes/accountingRoutes");
+const clientRoutes = require("./routes/clientRoutes");
+const usersRoutes = require("./routes/usersRoutes");
+const inventoryRoutes = require("./routes/inventoryRoutes");
+const invoiceRoutes = require("./routes/invoiceRoutes");
+const orderRoutes = require("./routes/orderRoutes");
+const permissionRoutes = require("./routes/permissionRoutes");
+const productRoutes = require("./routes/productRoutes");
+const roleRoutes = require("./routes/roleRoutes");
+const stockRoutes = require("./routes/stockRoutes");
+const supplierRoutes = require("./routes/supplierRoutes");
+const supplyRoutes = require("./routes/supplyRoutes");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
 const app = express();
 const PORT = process.env.PORT || 3090;
 
+// Conectar a MongoDB
+mongoose
+  .connect(config.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB conectado con Exito"))
+  .catch((err) => console.log(err));
+app.use(bodyParser.json());
+
 // Configura Swagger UI en la ruta /api-docs
 swaggerConfig(app);
 // Usar las rutas
-app.use('/api/accounting', accountingRoutes);
-app.use('/api/clients', clientRoutes);
-app.use('/api/users', usersRoutes);
-app.use('/api/inventory', inventoryRoutes);
-app.use('/api/invoices', invoiceRoutes);
-app.use('/api/orders', orderRoutes);
-app.use('/api/permissions', permissionRoutes);
-app.use('/api/products', productRoutes);
-app.use('/api/roles', roleRoutes);
-app.use('/api/stock', stockRoutes);
-app.use('/api/suppliers', supplierRoutes);
-app.use('/api/supplies', supplyRoutes);
+app.use("/api/accounting", accountingRoutes);
+app.use("/api/clients", clientRoutes);
+app.use("/api/users", usersRoutes);
+app.use("/api/inventory", inventoryRoutes);
+app.use("/api/invoices", invoiceRoutes);
+app.use("/api/orders", orderRoutes);
+app.use("/api/permissions", permissionRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/roles", roleRoutes);
+app.use("/api/stock", stockRoutes);
+app.use("/api/suppliers", supplierRoutes);
+app.use("/api/supplies", supplyRoutes);
 // Otras rutas y configuración de tu aplicación
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
 app.listen(PORT, () => {
--- config/config.js
+++ config/config.js
@@ -1,4 +1,31 @@
+const crypto = require('crypto');
+
+const algorithm = 'aes-256-cbc';
+const key = crypto.randomBytes(32); // Clave para cifrar
+const iv = crypto.randomBytes(16); // Vector de inicialización
+
+const decrypt = (encryptedText) => {
+  const decipher = crypto.createDecipheriv(algorithm, key, iv);
+  let decrypted = decipher.update(encryptedText, 'hex', 'utf8');
+  decrypted += decipher.final('utf8');
+  return decrypted;
+};
+
+const encrypt = (text) => {
+  const cipher = crypto.createCipheriv(algorithm, key, iv);
+  let encrypted = cipher.update(text, 'utf8', 'hex');
+  encrypted += cipher.final('hex');
+  return encrypted;
+};
+
+const uri = 'mongodb+srv://Daciros:K8MWGZvddF52pfgL@cluster0.bwvh0sb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+const encryptedUri = encrypt(uri);
+console.log('URI cifrada:', encryptedUri);
+
+//const encryptedUri = '50647975665485d4335218c47fc48835007c13217be85c19c1719874a6f31d03443ad07cef5b82cd3af1fa0659f3aa59a2a3ff66879dd9e39c6d2d38f118cb521e01addb7555618de3cb38b2edf92b15367b018fd753bbec098c184d0676848f9f1feffe4ac7f5ca6382dc37b09d664288f3247cebb201033b479ae62b555564'; // Usa el valor cifrado generado
+const decryptedUri = decrypt(encryptedUri);
+
 module.exports = {
-    mongoURI: 'mongodb+srv://cluster0.bwvh0sb.mongodb.net/portal_administracion',
+    mongoURI: decryptedUri,
     secretOrKey: 'secret'
   };
